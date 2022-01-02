c6e3914a7410297353ba1e3507952b3cc230669c
naama-grinwald <naama.grinwald@gmail.com>
2022-01-02 12:32:18 +0200

    flask scheme 2

--- app.py
+++ app.py
@@ -1,12 +1,11 @@
 from flask import Flask
 
 app = Flask(__name__)
-
+app.config.from_pyfile('settings.py')
 
 @app.route('/')
 def hello_world():  # put application's code here
     return 'Hello World!'
-
 
 if __name__ == '__main__':
     app.run()
